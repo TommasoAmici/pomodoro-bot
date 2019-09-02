1086749aa81bf5a8502aec676fe3091d093c75d4
Tommaso A <tommaso.amici@me.com>
2019-09-03 00:14:53 +0200

    added /stats and /total to show information

--- main.py
+++ main.py
@@ -54,9 +54,46 @@ def start_pomodoro(bot, update):
     )
 
 
-updater = Updater(os.environ["POMODORO_TELEGRAM_TOKEN"])
+def stats(update, context, total=False):
+    """
+    Returns stats for past seven days
+    """
+    chat_id = update.message.chat.id
+    hash_name = f"pomodoro:{chat_id}:*:list"
+
+    keys = r.scan(0, hash_name, 1000)[1]
+    pomodoros = [r.lrange(k, 0, -1) for k in keys]
+    # filter last seven days
+    last_week = date.today() - timedelta(days=7)
+    pomodoros = [
+        (
+            # pomodoros completed in last week
+            [p for p in pomodoro if (not total and date.fromisoformat(p) > last_week)],
+            # username
+            r.hget(keys[i][:-5], "username"),
+        )
+        for i, pomodoro in enumerate(pomodoros)
+    ]
+    # sort by number of pomodoros completed
+    pomodoros = sorted(pomodoros, key=lambda k: len(k[0]), reverse=True)
+    text = "\n".join([f"{p[1]}: {len(p[0])}" for p in pomodoros])
+    context.bot.send_message(
+        chat_id=chat_id, text=f"🍅 STATS POMODORI {date.today().isoformat()} 🍅\n{text}"
+    )
+
+
+def total(update, context):
+    """
+    Returns total stats
+    """
+    stats(update, context, total=True)
+
+
+updater = Updater(os.environ["POMODORO_TELEGRAM_TOKEN"], use_context=True)
 
 updater.dispatcher.add_handler(CommandHandler("pomodoro", start_pomodoro))
+updater.dispatcher.add_handler(CommandHandler("stats", stats))
+updater.dispatcher.add_handler(CommandHandler("total", stats))
 updater.dispatcher.add_handler(MessageHandler(Filters.all, check_if_cheating))
 
 updater.start_polling()
